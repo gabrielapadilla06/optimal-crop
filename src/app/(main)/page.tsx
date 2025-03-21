b1fe3936e192bade0f6befe95882b3e236b93941
Gabriela Padilla <gabriela.padilla.salgado@outlook.com>
2025-03-20 22:56:25 -0500

    Add main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,36 +3,35 @@
 import { TextBentoRight, StarIcon } from "@/components/ui/right-image"
 import { TextBentoLeft, ClockIcon } from "@/components/ui/left-image"
 import { HeroSection } from "@/components/ui/hero-section"
-import { FAQSection } from "@/components/ui/faqs"  
 
-import cristalesImage from "@/images/image.jpeg"
-import pinguinoImage from "@/images/image.jpeg"
+import heroImage from "@/images/image.jpeg"
+import feature1Image from "@/images/feature1.png"
+import feature2Image from "@/images/feature2.png"
+import feature3Image from "@/images/feature3.png"
 
 export default function Home() {
   return (
     <>
       <div className="mx-auto w-full max-w-screen-xl xl:pb-2">
         <HeroSection
-          title="Sustainabite"
-          subtitle="Creating the next generation of sustainable food"
+          title="Crop AI"
+          subtitle="Smart crop planning and care with AI"
           buttonText="Explore"
-          heroImage={cristalesImage.src}
+          heroImage={heroImage.src}
         />
 
         <div className="gap-10 p-4 py-10 sm:pb-4 md:flex md:justify-between">
           <TextBentoRight
-            title="Sustainabite"
-            subtitle="Meet"
-            description="an AI-powered tool to reduce food byproduct waste by usign them in new formulations."
-            buttonText="Learn More"
-            buttonHref="/solution"
+            title="Optimal Crop Prediction"
+            subtitle="Feature 1"
+            description="Predict the optimal crop, from 22 possible crops, based on weather and soil parameters"
             bentoCard={{
-              name: "Advanced Analytics",
-              description: "Gain insights into your work patterns and optimize your performance.",
-              backgroundSrc: pinguinoImage.src,
+              name: "Optimal Crop Prediction",
+              description: "Predict the optimal crop, from 22 possible crops, based on weather and soil parameters",
+              backgroundSrc: feature1Image.src,
               Icon: StarIcon,
               href: "#",
-              cta: "Explore Analytics",
+              cta: "Feature 1",
             }}
             reversed={true}
           />
@@ -41,21 +40,34 @@ export default function Home() {
         <div className="gap-4 p-4 sm:pb-4 md:flex md:justify-between">
           <TextBentoLeft
             bentoCard={{
-              name: "Smart Time Blocking",
-              description: "Automatically block time for focused work and personal activities.",
-              backgroundSrc: pinguinoImage.src,
+              name: "Chat",
+              description: "Ask any question to an AI and learn everything you need to take care of your crops.",
+              backgroundSrc: feature2Image.src,
               Icon: ClockIcon,
               href: "#",
-              cta: "Learn More",
+              cta: "Feature 2",
             }}
-            title="Did you know?"
-            subtitle="Problem"
-            description="Industrial food processing generates vast amounts of waste and by-products, often ranging between 30% to 60% by weight of the total production"
+            title="Chat"
+            subtitle="Feature 2"
+            description="Ask any question to an AI and learn everything you need to take care of your crops."
           />
         </div>
 
         <div className="gap-4 p-4 md:flex md:justify-between">
-          <FAQSection />
+          <TextBentoRight
+            title="Irrigation plan"
+            subtitle="Feature 3"
+            description="Create an irrigation plan based on the specific needs of your crops."
+            bentoCard={{
+              name: "Irrigation plan",
+              description: "Gain insights into your work patterns and optimize your performance.",
+              backgroundSrc: feature3Image.src,
+              Icon: StarIcon,
+              href: "#",
+              cta: "Feature 3",
+            }}
+            reversed={true}
+          />
         </div>
       </div>
     </>
